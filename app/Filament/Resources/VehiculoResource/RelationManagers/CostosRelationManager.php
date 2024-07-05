d5de5d7d9864465d0362121a29b7064739198895
SiamSoftware <siamsoftwarecol@gmail.com>
2024-07-05 15:58:15 -0500

    Ajuste Campo Editar

--- app/Filament/Resources/VehiculoResource/RelationManagers/CostosRelationManager.php
+++ app/Filament/Resources/VehiculoResource/RelationManagers/CostosRelationManager.php
@@ -14,6 +14,7 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Hidden;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Actions\Action;
 use Illuminate\Database\Eloquent\Builder;
@@ -95,7 +96,85 @@ class CostosRelationManager extends RelationManager
 
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                /* Tables\Actions\EditAction::make(), */
+
+                Action::make('Editar')
+                    ->form(function ($record) {
+                        return [
+                            // Campo oculto para el ID
+                            Hidden::make('id')
+                                ->default($record->id ?? null),
+
+                            Select::make('item_id')
+                                ->relationship('item', 'nombre')
+                                ->columnSpan(2)
+                                ->required()
+                                ->label('Repuesto')
+                                ->default($record->item_id ?? null), // Cargar valor existente
+                            TextInput::make('valor')
+                                ->columnSpan(3)
+                                ->prefix('$ ')
+                                ->minValue(0)
+                                ->maxValue(9999999999999)
+                                ->type('number')
+                                ->label('Valor')
+                                ->step('1')
+                                ->placeholder('0.00')
+                                ->default($record->valor ?? 0), // Cargar valor existente
+                            Textarea::make('descripcion')
+                                ->maxLength(255)
+                                ->autocomplete(false)
+                                ->columnSpan(4)
+                                ->label('Observaciones')
+                                ->markAsRequired(false)
+                                ->default($record->descripcion ?? ''), // Cargar valor existente
+                            FileUpload::make('ruta_imagen_item')
+                                ->label('Imagen del Repuesto')
+                                ->columnSpan(4)
+                                ->openable()
+                                ->downloadable()
+                                ->disk('spaces')
+                                ->directory('images')
+                                ->visibility('public')
+                                ->default($record->ruta_imagen_item ?? null), // Cargar valor existente
+                        ];
+                    })
+                    ->action(function (array $data, $livewire) {
+                        $vehiculo = Vehiculo::find($this->getOwnerRecord()->id);
+
+                        // Buscar el costo existente
+                        $costo = Costo::find($data['id']);
+
+                        // Calcular la diferencia y ajustar el total del costo del vehículo
+                        $valorAnterior = $costo ? $costo->valor : 0;
+                        $diferencia = $data['valor'] - $valorAnterior;
+
+                        // Si el costo existe, actualiza, de lo contrario, crea uno nuevo
+                        if ($costo) {
+                            $costo->update([
+                                'item_id' => $data['item_id'],
+                                'valor' => $data['valor'],
+                                'descripcion' => $data['descripcion'],
+                                'ruta_imagen_item' => $data['ruta_imagen_item'],
+                            ]);
+                        } else {
+                            $costo = Costo::create([
+                                'item_id' => $data['item_id'],
+                                'valor' => $data['valor'],
+                                'descripcion' => $data['descripcion'],
+                                'ruta_imagen_item' => $data['ruta_imagen_item'],
+                                'vehiculo_id' => $vehiculo->id,
+                            ]);
+                        }
+
+                        $sumatotal = $vehiculo->total_costo + $diferencia;
+                        $vehiculo->update([
+                            'total_costo' => $sumatotal,
+                        ]);
+
+                        $livewire->dispatch('refreshForm');
+                    }),
+
                 Action::make('Eliminar Repuesto')
                     ->requiresConfirmation()
                     ->action(
