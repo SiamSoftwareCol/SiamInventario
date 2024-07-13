730e709aa774670eb16e0a51285e3aa782adcaf5
Diego <diego01andres@hotmail.com>
2024-07-12 20:03:07 -0500

    Forzar el HTTPS en archivo AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+        if (app()->environment('local')) {
+            URL::forceScheme('https');
+        }
     }
+
+
 }
