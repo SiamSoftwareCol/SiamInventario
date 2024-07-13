c1c8a3caa77a167b6da463ab046e64d97001c11a
Diego <diego01andres@hotmail.com>
2024-07-12 20:06:49 -0500

    Forzar el HTTPS en archivo AppServiceProvider Reverso

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
-        if (app()->environment('local')) {
-            URL::forceScheme('https');
-        }
+
     }
 
 
